f528c609ef8d006d9b6ac07b0a827a5b237913d5
Chung Yun <134187582+Chung-Yun2@users.noreply.github.com>
2023-07-09 20:35:31 +0300

    Новый сабкласс "Любимка администрации"
    Signed-off-by: Chung Yun <134187582+Chung-Yun2@users.noreply.github.com>

---
+++ src/RuCollection.API/Subclasses/Single/AdminsLover.cs
@@ -0,0 +1,31 @@
+﻿using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RuCollection.API.Subclasses.Single
+{
+    internal class AdminsLover : SingleSubclass
+    {
+        private static AdminsLover _singleton;
+
+        private AdminsLover() { }
+
+        public static AdminsLover Singleton => _singleton ??= new();
+
+        public override string Name { get; } = "Любимка администрации";
+
+        public override RoleTypeId Role { get; } = RoleTypeId.ClassD;
+
+        public override string Message { get; } = "Вы - любимка администрации!\nУ вас маленький рост.";
+
+        public override string Color { get; } = "#009A63";
+
+        public override bool Show { get; } = true;
+
+        public override Vector3 Size { get; } = Vector3.one * 0.44f;
+    }
+}
