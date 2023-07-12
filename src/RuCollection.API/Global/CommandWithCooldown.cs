8bb704942f83bf37a8b40c5768ad8c6a0aa432ef
NotAloneAgain <maoffru@gmail.com>
2023-07-12 14:33:22 +0300

    Fix

--- src/RuCollection.API/Global/CommandWithCooldown.cs
+++ src/RuCollection.API/Global/CommandWithCooldown.cs
@@ -11,6 +11,11 @@ namespace RuCollection.API.Global
 
         public override bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
+            if (!base.Execute(arguments, sender, out response))
+            {
+                return false;
+            }
+
             var time = Mathf.RoundToInt((float)(Round.ElapsedTime.TotalSeconds - (LastUsed?[Executor] ?? 0)));
 
             if (time <= Cooldown)
@@ -19,7 +24,7 @@ namespace RuCollection.API.Global
                 return false;
             }
 
-            return base.Execute(arguments, sender, out response);
+            return true;
         }
     }
 }
